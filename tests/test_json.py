be517def7ad5332f8eae6e10eadae41244eff72e
Pavel Lipatov <pavel_lipatov@epam.com>
2020-03-18 13:50:50 +0300

    Add mock tests.

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,7 +1,9 @@
 import json
 import unittest
+from unittest.mock import Mock
 
 from app.convert_alert import convert_sd_to_ms
+from app import main
 
 class TestJSON(unittest.TestCase):
     
@@ -12,3 +14,11 @@ class TestJSON(unittest.TestCase):
             teams_json = convert_sd_to_ms(incoming_json)
             
             self.assertEqual(teams_valid_json, teams_json)
+
+
+    def test_cf_with_valid_sd_json(self):
+        with open("tests/sd_valid_incident_closed.json") as json_valid:
+            incoming_json = json.load(json_valid)
+            
+            req = Mock(get_json=Mock(return_value=incoming_json), args=incoming_json)
+            self.assertEqual(main.send_to_teams(req), "OK")
