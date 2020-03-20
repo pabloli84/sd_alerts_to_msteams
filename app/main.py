4ee1b8bf50c26f9e5c80da0c0d5ea50b12bba575
Pavel Lipatov <pavel_lipatov@epam.com>
2020-03-20 21:17:54 +0300

    Add mock server for webhook test.

--- app/main.py
+++ app/main.py
@@ -7,23 +7,20 @@ from exceptions import MissingTeamsWebhookConnector, AuthKeyNotValid
 
 app = Flask(__name__)
 ms_teams_webhook = os.getenv("MS_TEAMS_WEBHOOK", "none")
-if ms_teams_webhook == "none":
-    raise MissingTeamsWebhookConnector('Missing MS_TEAMS_WEBHOOK variable set.')
 
 
 def send_to_teams(request):
+    if ms_teams_webhook == "none":
+        raise MissingTeamsWebhookConnector('Missing MS_TEAMS_WEBHOOK variable set.')
+
     data = request.get_json(silent=True)
-    args = request.args
 
     teams_card = convert_sd_to_ms(data)
     print(data)
 
     print(teams_card)
 
-    if args['key'] == "94407572d3907543ba4a614b5e723ac2":
-        r = requests.post(ms_teams_webhook, json=teams_card)
-    else:
-        raise AuthKeyNotValid("Please specify valid key in the request")
+    r = requests.post(ms_teams_webhook, json=teams_card)
 
     print(f'Hook return code: {r.status_code}')
 
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,3 +1,11 @@
+# Standard library imports...
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+from threading import Thread
+import os
+# Third-party imports...
+import requests
+
 import json
 import unittest
 from unittest.mock import Mock
@@ -5,12 +13,25 @@ from unittest.mock import Mock
 from app.convert_alert import convert_sd_to_ms
 from app import main
 
+http_port = 6000
+os.environ['MS_TEAMS_WEBHOOK'] = f'http://localhost:{http_port}/'
+
+
+def run_mock_server():
+    mock_server = HTTPServer(('localhost', http_port), MockServerRequestHandler)
+
+    # Start running mock server in a separate thread.
+    # Daemon threads automatically shut down when the main process exits.
+    mock_server_thread = Thread(target=mock_server.serve_forever)
+    mock_server_thread.setDaemon(True)
+    mock_server_thread.start()
+
 
 class TestJSON(unittest.TestCase):
     
     def test_valid_input_sd_json(self):
-        with open("tests/sd_valid_incident_closed.json") as json_valid, \
-                open('tests/teams_valid_incident_closed.json') as json_teams:
+        with open("sd_valid_incident_closed.json") as json_valid, \
+                open('teams_valid_incident_closed.json') as json_teams:
             incoming_json = json.load(json_valid)
             teams_valid_json = json.load(json_teams)
             teams_json = convert_sd_to_ms(incoming_json)
@@ -18,10 +39,18 @@ class TestJSON(unittest.TestCase):
             self.assertEqual(teams_valid_json, teams_json)
 
     def test_cf_with_valid_sd_json(self):
-        with open("tests/sd_valid_incident_closed.json") as json_valid:
+        with open("sd_valid_incident_closed.json") as json_valid:
             incoming_json = json.load(json_valid)
-            key = "94407572d3907543ba4a614b5e723ac2"
-            data = {'key': key}
-            
-            req = Mock(get_json=Mock(return_value=incoming_json), args=data)
+
+            run_mock_server()
+
+            req = Mock(get_json=Mock(return_value=incoming_json))
             self.assertEqual(main.send_to_teams(req), "OK")
+
+
+class MockServerRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(requests.codes.ok)
+        self.end_headers()
+
+        return
